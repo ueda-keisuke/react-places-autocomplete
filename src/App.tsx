a585e48c2030e2033eff9c0930e69d0dffb7bb85
Keisuke Ueda <ueda.keisuke@me.com>
2024-04-20 22:58:46 -0700

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import PlacesAutocomplete, {
+    geocodeByAddress,
+    getLatLng,
+} from 'react-places-autocomplete';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [address, setAddress] = useState("");
+    const [coordinates, setCoordinates] = useState<{lat: number | null, lng: number | null}>({lat: null, lng: null});
+    const [addressComponents, setAddressComponents] = useState<google. maps. GeocoderAddressComponent[]>([]);
+
+
+    const handleSelect = async (value: string) => {
+        const results = await geocodeByAddress(value);
+        const latLng = await getLatLng(results[0]);
+        setAddress(value);
+        setCoordinates(latLng);
+        console.log(results);
+        setAddressComponents(results[0].address_components);
+    }
+
+    return (
+        <div className="App">
+            <PlacesAutocomplete
+                value={address}
+                onChange={setAddress}
+                onSelect={handleSelect}
+            >
+                {({getInputProps, suggestions, getSuggestionItemProps, loading}) => (
+                    <div>
+                        <p>Latitude: {coordinates.lat}</p>
+                        <p>Longitude: {coordinates.lng}</p>
+                        <p>Address: {addressComponents.map(ac => ac.long_name).join(', ')}</p>
+
+                        <input {...getInputProps({placeholder: "Type address"})} />
+                        <div>
+                            {loading ? <div>...loading</div> : null}
+
+                            {suggestions.map((suggestion, index) => {
+                                const style: React.CSSProperties = {
+                                    backgroundColor: suggestion.active ? "#41b6e6" : "#fff"
+                                };
+
+                                return (
+                                    <div {...getSuggestionItemProps(suggestion, {style})} key={index}>
+                                        {suggestion.description}
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </div>
+                )}
+            </PlacesAutocomplete>
+        </div>
+    );
 }
 
 export default App;
